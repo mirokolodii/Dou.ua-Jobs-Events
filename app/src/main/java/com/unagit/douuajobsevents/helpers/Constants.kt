720a1a7ce9bccfdd4b7ced6124104946789ce5a1
a264889 <mkolodii@gmail.com>
2018-10-08 14:42:54 +0200

    Refactor MainActivity.kt

--- app/src/main/java/com/unagit/douuajobsevents/helpers/Constants.kt
+++ app/src/main/java/com/unagit/douuajobsevents/helpers/Constants.kt
@@ -2,6 +2,7 @@ package com.unagit.douuajobsevents.helpers
 
 object TabsObj {
     const val TABS_COUNT = 2
+    const val TAB_BUNDLE_LABEL = "tabLabel"
     enum class Tabs(val pos: Int, val title: String) {
         TAB_1(0, "Events"),
         TAB_2(1, "Jobs")
--- app/src/main/java/com/unagit/douuajobsevents/views/MainActivity.kt
+++ app/src/main/java/com/unagit/douuajobsevents/views/MainActivity.kt
@@ -2,9 +2,8 @@ package com.unagit.douuajobsevents.views
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.support.v4.view.ViewPager
 import com.unagit.douuajobsevents.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,17 +15,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupTabs() {
-        val viewPager: ViewPager = findViewById(R.id.container_list)
-        viewPager.adapter = FragmentsAdapter(supportFragmentManager)
+//        val viewPager: ViewPager = findViewById(R.id.container_list)
+//        viewPager.adapter = FragmentsAdapter(supportFragmentManager)
+        container_list.adapter = FragmentsAdapter(supportFragmentManager)
 
-        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
-        tabLayout.setupWithViewPager(viewPager)
-
-        for(pos in 0..tabLayout.tabCount) {
-            tabLayout.getTabAt(pos)?.text = "title $pos"
+//        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+//        tabLayout.setupWithViewPager(viewPager)
+        tab_layout.setupWithViewPager(container_list)
+        // Set tab titles
+        for(pos in 0 until tab_layout.tabCount) {
+            tab_layout.getTabAt(pos)?.text = "title $pos"
         }
-
-
     }
-
 }
