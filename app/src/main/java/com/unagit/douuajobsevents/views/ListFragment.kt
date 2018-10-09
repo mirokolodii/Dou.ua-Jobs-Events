4a6eb24c773edef5d5230b0a30a24de9c7959c72
a264889 <mkolodii@gmail.com>
2018-10-09 17:18:32 +0200

    Cleanup so that app can compile.

--- app/src/main/java/com/unagit/douuajobsevents/views/ListFragment.kt
+++ app/src/main/java/com/unagit/douuajobsevents/views/ListFragment.kt
@@ -9,22 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.unagit.douuajobsevents.R
+import com.unagit.douuajobsevents.data.Item
 import com.unagit.douuajobsevents.presenters.ListPresenter
 
 
-class ListFragment : Fragment(), ListPresenter.ListView {
-    private val presenter = ListPresenter()
-
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-        presenter.attach(this)
-    }
-
-
-    override fun onDestroy() {
-        super.onDestroy()
-        presenter.detach()
-    }
+class ListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_list, container, false)
@@ -32,11 +21,4 @@ class ListFragment : Fragment(), ListPresenter.ListView {
         return rootView
     }
 
-    override fun showLoading() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun showItems() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 }
