026a31f041fa338e6738e47179e63432b145ee54
Myroslav Kolodii <mkolodii@gmail.com>
2018-10-19 22:48:28 +0200

    Add DetailsActivity.

---
+++ app/src/main/java/com/unagit/douuajobsevents/views/DetailsActivity.kt
@@ -0,0 +1,15 @@
+package com.unagit.douuajobsevents.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.unagit.douuajobsevents.R
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+
+    }
+}
