5f806e02f825a4688febadf19c040268af8c44d0
Myroslav Kolodii <mkolodii@gmail.com>
2018-10-23 12:46:18 +0200

    Add scrollable toolbar in activity_details layout.

--- app/src/main/java/com/unagit/douuajobsevents/views/DetailsActivity.kt
+++ app/src/main/java/com/unagit/douuajobsevents/views/DetailsActivity.kt
@@ -2,6 +2,7 @@ package com.unagit.douuajobsevents.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.unagit.douuajobsevents.R
 
 class DetailsActivity : AppCompatActivity() {
@@ -9,6 +10,9 @@ class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
 
     }
