bb08753a65e21ea9b76a76e9718cd952cacd2c1b
Myroslav Kolodii <mkolodii@gmail.com>
2018-10-24 19:45:53 +0200

    Decode item's description in DetailsActivity.

--- app/src/main/java/com/unagit/douuajobsevents/views/DetailsActivity.kt
+++ app/src/main/java/com/unagit/douuajobsevents/views/DetailsActivity.kt
@@ -1,6 +1,8 @@
 package com.unagit.douuajobsevents.views
 
 import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.text.HtmlCompat
@@ -17,19 +19,28 @@ class DetailsActivity : AppCompatActivity() {
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayShowTitleEnabled(false) //remove title
 
         // Get Item from guid
         val guid = intent.getStringExtra(getString(R.string.extra_guid_id))
         val item = DataInjector.getItemById(guid)
 
         itemTitle.text = item.title
-//        val spannedHtml = HtmlCompat.fromHtml(item.description, HtmlCompat.FROM_HTML_MODE_LEGACY)
-        itemDetails.text = item.description //spannedHtml
+        val description = decodeHtml(item.description)
+        itemDetails.text = description
 
-        itemTitle.setOnClickListener {_ ->
+        itemTitle.setOnClickListener { _ ->
             Snackbar.make(activityDetailsLayout, item.title, Snackbar.LENGTH_SHORT).show()
         }
+    }
 
+    private fun decodeHtml(str: String): Spanned {
+        var result = HtmlCompat.fromHtml(str, HtmlCompat.FROM_HTML_MODE_COMPACT)
+
+        while(result.toString() != str) {
+            result = HtmlCompat.fromHtml(str, HtmlCompat.FROM_HTML_MODE_COMPACT)
+        }
+
+        return result
     }
 }
