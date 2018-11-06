f62a16585288acebe0a8a59b16bc30254f204bc7
Myroslav Kolodii <mkolodii@gmail.com>
2018-11-06 09:12:15 +0100

    Add Room annotations in Item.kt.

--- app/src/main/java/com/unagit/douuajobsevents/models/Item.kt
+++ app/src/main/java/com/unagit/douuajobsevents/models/Item.kt
@@ -1,10 +1,19 @@
 package com.unagit.douuajobsevents.models
 
 import android.text.Spanned
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.unagit.douuajobsevents.helpers.ItemType
 
-data class Item(val title: String,
-                val guid: String,
-                val type: ItemType,
-                val imgUrl: String,
-                val description: Spanned)
+@Entity(tableName = "entity_table")
+data class Item(
+        @PrimaryKey
+        @NonNull
+        var guid: String,
+
+        var title: String,
+        var type: ItemType,
+        var imgUrl: String,
+        var description: Spanned
+)
