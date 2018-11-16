4231ff91156ab08b5622f7ec6fc487f46969da5a
Myroslav Kolodii <mkolodii@gmail.com>
2018-11-16 16:56:17 +0100

    Register RefreshAlarmReceiver in manifest.

--- app/src/main/java/com/unagit/douuajobsevents/services/RefreshAlarmReceiver.kt
+++ app/src/main/java/com/unagit/douuajobsevents/services/RefreshAlarmReceiver.kt
@@ -3,11 +3,13 @@ package com.unagit.douuajobsevents.services
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.unagit.douuajobsevents.R
 
 class RefreshAlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        val serviceIntent = Intent(context, RefreshService::class.java)
-        context?.startService(serviceIntent)
+//        if (intent != null && intent.action == context?.getString(R.string.refresh_alarm_receiver_action)) {
+            context?.startService(Intent(context, RefreshService::class.java))
+//        }
     }
 
 }
