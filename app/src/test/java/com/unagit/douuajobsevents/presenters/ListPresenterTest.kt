268f56394b0c0d36d6c426ee171b95b3927e20a7
Myroslav Kolodii <mkolodii@gmail.com>
2019-01-03 11:51:12 +0100

    Add Truth library test dependency

--- app/src/test/java/com/unagit/douuajobsevents/presenters/ListPresenterTest.kt
+++ app/src/test/java/com/unagit/douuajobsevents/presenters/ListPresenterTest.kt
@@ -1,15 +1,21 @@
 package com.unagit.douuajobsevents.presenters
 
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
-import org.junit.Assert.*
-
 class ListPresenterTest {
+
+
+    @Test
+    fun `sum of 1 and 2 should equal 3`() {
+        val sum = 1 + 2
+        assertThat(sum).isEqualTo(3)
+
+    }
 
     @Test
     fun attach() {
         val result = 1 + 2
-        assertEquals(result, 3)
     }
 
     @Test
